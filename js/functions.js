c86203e1d6794be86ca9bd99516dcd185dbd6387
teplostanski <igor@teplostan.ski>
2025-10-14 00:31:47 +0300

    Добавляет функцию для извлечения цифр от 0 до 9

--- js/functions.js
+++ js/functions.js
@@ -34,3 +34,19 @@ console.debug(isPalindrome("hello")); // false
 console.debug(isPalindrome("Was it a car or a cat I saw?")); // true
 console.debug(isPalindrome("А роза упала на лапу Азора.")); // true
 console.groupEnd();
+
+const extractDigits = (arg) => {
+  const digits = arg.toString().match(/\d+/g);
+  return digits ? parseInt(digits.join(""), 10) : NaN;
+};
+
+console.group("extractDigits");
+console.debug(extractDigits("2023 год")); // 2023
+console.debug(extractDigits("ECMAScript 2022")); // 2022
+console.debug(extractDigits("1 кефир, 0.5 батона")); // 105
+console.debug(extractDigits("агент 007")); // 7
+console.debug(extractDigits("а я томат")); // NaN
+console.debug(extractDigits(2023)); // 2023
+console.debug(extractDigits(-1)); // 1
+console.debug(extractDigits(1.5)); // 15
+console.groupEnd();
