61b2f06dcf021b15beb3ca24902836128fc1e91a
teplostanski <igor@teplostan.ski>
2025-10-14 00:28:39 +0300

    Добавляет функцию для проверки длины строки

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,9 @@
-console.debug("работает");
+const checkStringLength = (str, targetLength) => {
+  return str.length <= targetLength;
+};
+
+console.group("checkStringLength");
+console.debug(checkStringLength("Кот", 10)); // true
+console.debug(checkStringLength("Программирование на JavaScript", 15)); // false
+console.debug(checkStringLength("Код", 3)); // true
+console.groupEnd();
