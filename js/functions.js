b8530ae366af815d347ab249911f4a86c2329413
teplostanski <igor@teplostan.ski>
2025-10-14 00:40:24 +0300

    Исправляет ошибки линтинга

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,11 @@
-const checkStringLength = (str, targetLength) => {
-  return str.length <= targetLength;
-};
+const checkStringLength = (str, targetLength) => str.length <= targetLength;
 
-console.group("checkStringLength");
-console.debug(checkStringLength("Кот", 10)); // true
-console.debug(checkStringLength("Программирование на JavaScript", 15)); // false
-console.debug(checkStringLength("Код", 3)); // true
-console.groupEnd();
+checkStringLength('Кот', 10); // true
+checkStringLength('Программирование на JavaScript', 15); // false
+checkStringLength('Код', 3); // true
 
 const isPalindrome = (str) => {
-  const clearStr = str.replace(/[^a-zA-Zа-яА-Я0-9]/g, "").toLowerCase();
+  const clearStr = str.replace(/[^a-zA-Zа-яА-Я0-9]/g, '').toLowerCase();
 
   let left = 0;
   let right = clearStr.length - 1;
@@ -26,27 +22,23 @@ const isPalindrome = (str) => {
   return true;
 };
 
-console.group("isPalindrome");
-console.debug(isPalindrome("топот")); // true
-console.debug(isPalindrome("level")); // true
-console.debug(isPalindrome("привет")); // false
-console.debug(isPalindrome("hello")); // false
-console.debug(isPalindrome("Was it a car or a cat I saw?")); // true
-console.debug(isPalindrome("А роза упала на лапу Азора.")); // true
-console.groupEnd();
+isPalindrome('топот'); // true
+isPalindrome('level'); // true
+isPalindrome('привет'); // false
+isPalindrome('hello'); // false
+isPalindrome('Was it a car or a cat I saw?'); // true
+isPalindrome('А роза упала на лапу Азора.'); // true
 
 const extractDigits = (arg) => {
   const digits = arg.toString().match(/\d+/g);
-  return digits ? parseInt(digits.join(""), 10) : NaN;
+  return digits ? parseInt(digits.join(''), 10) : NaN;
 };
 
-console.group("extractDigits");
-console.debug(extractDigits("2023 год")); // 2023
-console.debug(extractDigits("ECMAScript 2022")); // 2022
-console.debug(extractDigits("1 кефир, 0.5 батона")); // 105
-console.debug(extractDigits("агент 007")); // 7
-console.debug(extractDigits("а я томат")); // NaN
-console.debug(extractDigits(2023)); // 2023
-console.debug(extractDigits(-1)); // 1
-console.debug(extractDigits(1.5)); // 15
-console.groupEnd();
+extractDigits('2023 год'); // 2023
+extractDigits('ECMAScript 2022'); // 2022
+extractDigits('1 кефир, 0.5 батона'); // 105
+extractDigits('агент 007'); // 7
+extractDigits('а я томат'); // NaN
+extractDigits(2023); // 2023
+extractDigits(-1); // 1
+extractDigits(1.5); // 15
